bda527927d0a0717befddfa2444efeb4ab6ab994
baedan <anarchotransformer@proton.me>
2023-10-23 18:29:13 +0800

    relaxed signature for `zone` to `AbstractEntity`

--- src/utilities/utilities.jl
+++ src/utilities/utilities.jl
@@ -4,14 +4,14 @@ using ..Concepts, Overseer
 
 players(m::AbstractLedger) = [player for player in @entities_in(m, PlayerComponent)]
 
-function zone(m::AbstractLedger, type::ZoneType, owner::Union{Entity, Nothing}=nothing) end
+function zone(m::AbstractLedger, type::ZoneType, owner::Union{Overseer.AbstractEntity, Nothing}=nothing) end
 function zone(m::AbstractLedger, type::UnownedZoneType, ::Nothing)
     game_info = Entity(m[GameInfo], 1)
     shortcut = m[game_info][ZoneShortcut{UnownedZoneType}]
-    return shortcut[type]
+    return (shortcut.zone_shortcut)[type]
 end
-function zone(m::AbstractLedger, type::OwnedZoneType, owner::Entity)
+function zone(m::AbstractLedger, type::OwnedZoneType, owner::Overseer.AbstractEntity)
     shortcut = m[owner][ZoneShortcut{OwnedZoneType}]
-    return shortcut[type]
+    return (shortcut.zone_shortcut)[type]
 end
 end
