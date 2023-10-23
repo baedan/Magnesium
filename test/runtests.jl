9c9aa564356fbf6f089ae16bb80503fbc86a7b58
baedan <anarchotransformer@proton.me>
2023-10-23 18:01:59 +0800

    added player name test for game initialization

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,33 @@
-using Magnesium
-using Test
 using Aqua
+using Magnesium
+using Magnesium.Concepts
+using Magnesium.Systems
+using Magnesium.Utilities
+using Test
+using Overseer
 
 @testset "Magnesium.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
+#=     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(Magnesium)
-    end
+    end =#
     # Write your tests here.
+
+
 end
+
+@testset "Initialize Game" begin
+    function test_InitializeGame(players::AbstractArray{PlayerInfo})
+        l = Ledger()
+        for player in players
+            Entity(l, player)
+        end
+        return Overseer.update(InitializeGame(), l)
+    end
+
+    @testset let h = test_InitializeGame(
+            [PlayerInfo(DeckList(), "Malatesta")])
+        @test h[first(players(h))][PlayerComponent] == PlayerComponent("Malatesta")
+    end
+
+end
+
